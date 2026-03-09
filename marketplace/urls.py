61aeaba824d82626eb1a6cd84b1b575d6de48ca2
2889073s <2889073S@student.gla.ac.uk>
2026-03-09 20:43:48 +0000

    Add initial seller and profile pages with views, URLs and initial templates

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -6,4 +6,6 @@ app_name = 'marketplace'
 urlpatterns = [
     path('', views.home, name='home'),
     path('seller/<slug:seller_username_slug>/<slug:product_name_slug>/', views.product_page, name = 'product_page'),
+    path('seller/<slug:seller_username_slug>/', views.seller_page, name = 'seller_page' ),
+    path('profile/', views.profile_page, name= 'profile_page')
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from marketplace.models import Product
+from marketplace.models import Product, UserProfile 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -20,3 +21,37 @@ def product_page(request,  seller_username_slug, product_name_slug):
     except Product.DoesNotExist:
         context_dict['product'] = None
     return render(request, 'marketplace/product.html', context= context_dict)
+
+
+def seller_page(request, seller_username_slug):
+    context_dict = {}
+    try:
+        #if we cant find the .get() method for this product id then rasies a DoesNotExist exeption
+        seller = UserProfile.objects.get(slug = seller_username_slug )
+        context_dict["seller"] = seller
+        sellers_products = Product.objects.filter(seller = seller, is_sold = False)
+        context_dict["products"] = sellers_products
+    except UserProfile.DoesNotExist:
+        context_dict['seller'] = None
+        context_dict["products"] = None
+    return render(request, 'marketplace/seller.html', context= context_dict)
+
+@login_required
+def profile_page(request):
+    profile = UserProfile.objects.get(user = request.user )
+    #gets the products that the seller is actively selling and ones that have been sold and products that they have bought
+    active_products = Product.objects.filter( seller = profile, is_sold = False)
+    sold_products = Product.objects.filter( seller = profile, is_sold = True)
+    bought_products = Product.objects.filter( buyer = profile)
+
+    context_dict = {
+        "profile" : profile,
+        "active_products" : active_products,
+        "sold_products" : sold_products,
+        "bought_products" : bought_products
+    }
+    return render(request, 'marketplace/profile.html', context= context_dict)
+
+
+
+
