89f136bd56d98d75f6e90d4fa9fee076c90f17e2
2889073s <2889073S@student.gla.ac.uk>
2026-03-09 22:14:12 +0000

    add rest of  marketplace URLs, intial views and templates

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -7,5 +7,15 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('seller/<slug:seller_username_slug>/<slug:product_name_slug>/', views.product_page, name = 'product_page'),
     path('seller/<slug:seller_username_slug>/', views.seller_page, name = 'seller_page' ),
-    path('profile/', views.profile_page, name= 'profile_page')
+    path('profile/', views.profile_page, name= 'profile_page'),
+    path('register/', views.user_register,  name = 'user_register'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('sell', views.sell_page, name = 'sell_page'),
+    path('search/', views.search_page, name = 'search_page'),
+    path('review/<slug:seller_username_slug>/', views.review_page, name = 'review_page'),
+    path('profile/edit-product/<slug:product_name_slug>/', views.edit_product_page, name="edit_product_page"),
+    path('profile/delete-product/<slug:product_name_slug>/', views.delete_product_page, name="delete_product_page"),
+
+
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from marketplace.models import Product, UserProfile 
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
@@ -51,6 +51,56 @@ def profile_page(request):
         "bought_products" : bought_products
     }
     return render(request, 'marketplace/profile.html', context= context_dict)
+
+#Ive just created the basic views for the forms
+def user_register(request):
+    context_dict = {}
+    return render(request, 'marketplace/register.html', context= context_dict)
+
+
+def user_login(request):
+    context_dict = {}
+    return render(request, 'marketplace/login.html', context= context_dict)
+
+@login_required
+def user_logout(request):
+    return redirect(reverse('marketplace:home'))
+
+def search_page(request):
+    context_dict = {}
+    return render(request, 'marketplace/search.html', context=context_dict)
+
+def sell_page(request):
+    context_dict = {}
+    return render(request, 'marketplace/sell.html', context=context_dict)
+
+
+def review_page(request, seller_username_slug):
+    context_dict = {}
+    return render(request, 'marketplace/review.html', context=context_dict)
+
+@login_required
+def edit_product_page(request, product_name_slug):
+    profile = UserProfile.objects.get(user=request.user)
+    context_dict = {}
+    try:
+        product = Product.objects.get(seller=profile, slug=product_name_slug)
+        context_dict["product"] = product
+    except Product.DoesNotExist:
+        context_dict["product"] = None
+    return render(request, 'marketplace/edit_product.html', context=context_dict)
+
+@login_required
+def delete_product_page(request, product_name_slug):
+    profile = UserProfile.objects.get(user=request.user)
+    context_dict = {}
+    try:
+        product = Product.objects.get(seller=profile, slug=product_name_slug)
+        context_dict["product"] = product
+    except Product.DoesNotExist:
+        context_dict["product"] = None
+    return render(request, 'marketplace/delete_product.html', context=context_dict)
+
 
 
 
